f5e28369d9f89fa76d9af4de5db6cfb861f47a18
Pedroporto87 <pedroporto_8@hotmail.com>
2024-08-21 18:24:05 -0300

    arrumando arquivos dos logos

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -1,16 +1,18 @@
 import '../styles/component/navbar.scss'
+import RMH from  '../assets/rmh.jpeg'
+import Psind from '../assets/psind-ce.jpeg'
 
 export const NavBar = () => {
     return (
         <nav className="navbar">
           <div className="navbar-left">
-            <img src="../../public/rmh.jpeg" alt="Left Logo" className="navbar-image" />
+            <img src={RMH} alt="Left Logo" className="navbar-image" />
           </div>
           <div className="navbar-title">
             Cadastro dos Psicólogos do Estado do Ceará
           </div>
           <div className="navbar-right">
-            <img src="../../public/psind-ce.jpeg" alt="Right Logo" className="navbar-image" />
+            <img src={Psind} alt="Right Logo" className="navbar-image" />
           </div>
         </nav>
       );
