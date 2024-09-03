9fa40497e8d1e88812dc7cedf00a7081e45e63c6
Pedroporto87 <pedroporto_8@hotmail.com>
2024-09-03 13:35:01 -0300

    ajustando resposta de erro

--- src/helpers/formService.js
+++ src/helpers/formService.js
@@ -2,13 +2,15 @@ import axios from 'axios';
 
 const API_URL = 'https://form-psind-backend-89b9de53d3ee.herokuapp.com/api/forms';
 
-
 export const submitForm = async (formData) => {
   try {
     const response = await axios.post(API_URL, formData);
     return response.data;
   } catch (error) {
-    console.error('Erro ao enviar formulário:', error);
-    throw error;
+    if (error.response && error.response.data && error.response.data.message) {
+      throw new Error(error.response.data.message);
+    } else {
+      throw new Error('Erro ao enviar formulário.');
+    }
   }
 };
