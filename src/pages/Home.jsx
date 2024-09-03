1b350fd2390ea3cb9a4f88496ee3e202704f5bac
Pedroporto87 <pedroporto_8@hotmail.com>
2024-09-03 18:14:24 -0300

    ajustando mensagem de erro

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,15 +28,13 @@ const Home = () => {
   const [filteredCidades, setFilteredCidades] = useState([]);
   const [showSuggestions, setShowSuggestions] = useState(false);
   const [extraFields, setExtraFields] = useState([]);
-  const [errorMessage, setErrorMessage] = useState('');
-
   useEffect(() => {
     const fetchData = async () => {
       try {
         const cidadesData = await fetchCidades();
         setCidades(cidadesData);
       } catch (error) {
-        setErrorMessage('Erro ao buscar cidades:', error);
+        alert('Erro ao buscar cidades:', error);
       }
     };
 
@@ -92,10 +90,10 @@ const Home = () => {
       alert('Por favor, preencha todos os campos obrigatórios.');
       return;
     }
-    setErrorMessage('');
+    
     const crpRegex = /^\d{6}$/;
     if (!crpRegex.test(formData.crp)) {
-      setErrorMessage('O CRP deve conter exatamente 6 números.');
+      alert('O CRP deve conter exatamente 7 números.');
       return;
     }
 
@@ -150,7 +148,6 @@ const Home = () => {
   return (
     <section className='formulario'> 
       <form onSubmit={handleSubmit}>
-       {errorMessage && <div className="error-message">{errorMessage}</div>}
         <input type="text" name="nome" value={formData.nome} onChange={handleChange} placeholder="Nome" required />
         <input type="email" name="email" value={formData.email} onChange={handleChange} placeholder="E-mail" required />
         <input type="tel" name="telefone" value={formData.telefone} onChange={handleChange} placeholder="Telefone" required />
